bdb9d3711b7162d64ca4d03f22ea58b0f11c78fc
Briggs Francis <briggsf22@gmail.com>
2023-07-16 21:36:48 +0100

    ran collect static and adjusted the templates

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,18 @@ set -e
 source function.sh 
 
 PORT=${PORT:-8000}
-workers=4
+workers=1
 project_name=age_calc
 error_msg="Couldn't runserver"
 
-# This script is for running the server 
 
+# This script is handling static static files
+notify info "==========Collecting Static Files================"
+python3 manage.py collectstatic --noinput
+
+
+
+# This script is for running the server 
 notify info "===========Running Server==========="
 gunicorn --bind=0.0.0.0:${PORT} \
          --workers=${workers} \
